f1b365d62d701fcdc52976419a219c06bc9bc69e
aman-kumar-official <amankumar.singh2023@vitstudent.ac.in>
2025-06-07 20:30:36 +0530

    currected rooms page

--- contact.php
+++ contact.php
@@ -5,13 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TripOnTheGo - CONTACT</title>
     <?php require('inc/links.php'); ?>
-    <style>
-        .pop:hover{
-            border-top-color: var(--teal) !important;
-            transform:scale(1.03);
-            transition: all 0.3s;
-        }
-    </style>
 </head>
 
 <body class="bg-light">
