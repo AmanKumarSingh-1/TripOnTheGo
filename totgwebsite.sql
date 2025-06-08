c1d8314dd2d1dee4e0552d5f75b631c825cb02b7
aman-kumar-official <amankumar.singh2023@vitstudent.ac.in>
2025-06-08 19:37:03 +0530

    adding sample db

--- totgwebsite.sql
+++ totgwebsite.sql
@@ -1,1 +1,12 @@
-SELECT NOW();
+CREATE DATABASE IF NOT EXISTS admin_cred;
+
+USE admin_cred;
+
+CREATE TABLE admin_cred (
+    sl_no INT AUTO_INCREMENT PRIMARY KEY,
+    admin_name VARCHAR(150),
+    admin_pass VARCHAR(150)
+);
+
+INSERT INTO admin_cred (admin_name, admin_pass)
+VALUES ('aman', 'aman9162');
