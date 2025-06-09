2525f0dc343bcdbf40a512c8aec6c885cdfa8e5e
sima mahato <mahatosima2004@gmail.com>
2025-06-09 21:40:02 +0530

    Setting Part-1

---
+++ tempCodeRunnerFile.php
@@ -0,0 +1,11 @@
+<?php
+<h5>Follow Us</h5>
+                    <a class="d-inline-block mb-3" href="#">
+                        <span class="badge bg-light text-dark fs-6 p-2"><i class="bi bi-facebook"></i> Facebook</span>
+                    </a><br>
+                    <a class="d-inline-block mb-3" href="#">
+                        <span class="badge bg-light text-dark fs-6 p-2"><i class="bi bi-instagram"></i> Instagram</span>
+                    </a><br>
+                    <a class="d-inline-block" href="#">
+                        <span class="badge bg-light text-dark fs-6 p-2"><i class="bi bi-twitter-x"></i> <i class="bi bi-twitter-x"></i></span>
+                    </a><br>
