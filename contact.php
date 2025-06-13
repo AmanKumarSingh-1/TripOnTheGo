8f2bcf641f2fb3f6bb525bc4b69d49cc30822411
Aman Kumar Singh <136615596+AmanKumarSingh-1@users.noreply.github.com>
2025-06-13 16:01:48 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -14,9 +14,7 @@
 
     <?php require('inc/header.php'); ?>
 
-    <div class="mg-5 px-4">
-        <br>
-        <br>
+    <div class="my-5 px-4">
         <h2 class="fw-bold h-font text-center">CONTACT US</h2>
         <div class="h-line bg-dark"></div>
         <p class="text-center mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quae, odit possimus incidunt harum facilis libero vel voluptates voluptate labore nemo esse obcaecati error magni ipsum porro aliquid distinctio id?
