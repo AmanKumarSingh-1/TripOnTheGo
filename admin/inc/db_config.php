c8b085711806d19bff7285ff703a2c97473b0244
Aman Kumar Singh <136615596+AmanKumarSingh-1@users.noreply.github.com>
2025-06-13 16:32:10 +0530

    Add files via upload

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -1,5 +1,4 @@
 <?php
-
     $hname = 'localhost';
     $uname = 'root';
     $pass = '';
@@ -19,6 +18,13 @@
             $data[$key] = strip_tags($value);
         }
         return $data;
+    }
+
+    function selectAll($table)
+    {
+        $con=$GLOBALS['con'];
+        $res=mysqli_query($con,"SELECT * FROM $table");
+        return $res;
     }
 
     function select($sql, $values, $datatypes) {
@@ -60,6 +66,48 @@
         }
         else {
             die("Query cannot be prepared - Update");
+        }
+    }
+
+    function insert($sql, $values, $datatypes) 
+    {
+        $con = $GLOBALS['con'];
+        if($stmt = mysqli_prepare($con, $sql)) 
+        {
+            mysqli_stmt_bind_param($stmt, $datatypes, ...$values);
+            if(mysqli_stmt_execute($stmt)) {
+                $res = mysqli_stmt_affected_rows($stmt);
+                mysqli_stmt_close($stmt);
+                return $res;
+            }
+            else {
+                mysqli_stmt_close($stmt);
+                die("Query cannot be executed - Insert");
+            }
+        }
+        else {
+            die("Query cannot be prepared - Insert");
+        }
+    }
+
+    function delete($sql, $values, $datatypes) 
+    {
+        $con = $GLOBALS['con'];
+        if($stmt = mysqli_prepare($con, $sql)) 
+        {
+            mysqli_stmt_bind_param($stmt, $datatypes, ...$values);
+            if(mysqli_stmt_execute($stmt)) {
+                $res = mysqli_stmt_affected_rows($stmt);
+                mysqli_stmt_close($stmt);
+                return $res;
+            }
+            else {
+                mysqli_stmt_close($stmt);
+                die("Query cannot be executed - Delete");
+            }
+        }
+        else {
+            die("Query cannot be prepared - Delete");
         }
     }
 
