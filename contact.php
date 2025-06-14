3dc307ca9feebeb99c3bfd5e65d4f310506d01c1
Aman Kumar Singh <contact.amankumarsingh@gmail.com>
2025-06-14 10:58:26 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -14,47 +14,78 @@
 
     <?php require('inc/header.php'); ?>
 
-    <div class="my-5 px-4">
+    <div class="mg-5 px-4">
+        <br>
+        <br>
         <h2 class="fw-bold h-font text-center">CONTACT US</h2>
         <div class="h-line bg-dark"></div>
         <p class="text-center mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quae, odit possimus incidunt harum facilis libero vel voluptates voluptate labore nemo esse obcaecati error magni ipsum porro aliquid distinctio id?
         </p>
     </div>
 
+
+
     <div class="container">
         <div class="row">
             <div class="col-lg-6 col-md-6 mb-5 px-4">
                 <div class="bg-white rounded shadow p-4">
-                    <iframe class="w-100 rounded mb-4" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117711.79652321864!2d86.09336889558041!3d22.784165518854753!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f5e31989f0e2b5%3A0xeeec8e81ce9b344!2sJamshedpur%2C%20Jharkhand!5e0!3m2!1sen!2sin!4v1749109966243!5m2!1sen!2sin" height="320" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe class="w-100 rounded mb-4" src="<?php echo $contact_r['iframe'] ?>" height="320" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
                     <h5>Address</h5>
-                    <a href="https://maps.app.goo.gl/zrGTS44hNgUrE9ke9" target="blank" class="d-inline-block text-decoration-none text-dark" mb-4>
-                        <i class="bi bi-geo-alt-fill"></i> Tata Motors Ltd., Jamshedpur
+                    <a href="<?php echo $contact_r['gmap'] ?>" target="blank" class="d-inline-block text-decoration-none text-dark" mb-4>
+                        <i class="bi bi-geo-alt-fill"></i> <?php echo $contact_r['address'] ?>
                     </a>
 
                     <!-- CALL US -->
 
                     <h5 class="mt-4">Call Us</h5>
-                    <a href="tel: +919876543210" class="d-inline-block mb-2 text-decoration-none text-dark">
-                        <i class="bi bi-telephone-fill"></i> +91 9876543210
+                    <a href="tel: +<?php echo $contact_r['pn1'] ?>" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <i class="bi bi-telephone-fill"></i> +<?php echo $contact_r['pn1'] ?>
                     </a>
                     <br>
-                    <a href="tel: +919876543210" class="d-inline-block mb-2 text-decoration-none text-dark">
-                        <i class="bi bi-telephone-fill"></i> +91 9876543210
-                    </a>
+
+                    <?php
+                    if ($contact_r['pn2'] != '') {
+                        echo <<<data
+                            
+                            <a href="tel: +$contact_r[pn2]" class="d-inline-block mb-2 text-decoration-none text-dark">
+                            <i class="bi bi-telephone-fill"></i> +$contact_r[pn2]
+                            </a>    
+                            data;
+                    }
+                    ?>
+
 
                     <h5 class="mt-4">Email</h5>
-                    <a href="mail to : ask.totgdev@gmail.com" class="d-inline-block mb-2 text-decoration-none text-dark">
-                        <i class="bi bi-envelope-fill"></i> ask.totgdev@gmail.com</a>
+                    <a href="mail to : <?php echo $contact_r['email'] ?>" class="d-inline-block mb-2 text-decoration-none text-dark">
+                        <i class="bi bi-envelope-fill"></i> <?php echo $contact_r['email'] ?> </a>
 
                     <h5 class="mt-4">Follow Us</h5>
-                    <a href="#" class="d-inline-block text-dark fs-5 me-2">
+
+                    <?php
+                    if ($contact_r['twitter'] != '') {
+                        echo <<<data
+                        
+                        <a href="$contact_r[twitter]" class="d-inline-block text-dark fs-5">
+                        <i class="bi bi-twitter"></i>
+                        </a>
+                        
+                        data;
+                    } ?>
+
+                    <?php
+                    if ($contact_r['twitter'] != '') {
+                        echo <<<data
+                        
+                        <a href="$contact_r[facebook]" class="d-inline-block text-dark fs-5 me-2">
                         <i class="bi bi-facebook"></i>
-                    </a>
+                        </a>
+                        
+                        data;
+                    } ?>
+
                     <a href="#" class="d-inline-block text-dark fs-5 me-2">
                         <i class="bi bi-instagram"></i>
-                    </a>
-                    <a href="#" class="d-inline-block text-dark fs-5">
-                        <i class="bi bi-twitter"></i>
                     </a>
 
                 </div>
@@ -66,26 +97,25 @@
                         <h5>Send a message</h5>
                         <div class="mb-3">
                             <label class="form-label">Name</label>
-                        <input type="text" class="form-control shadow-none">
+                            <input type="text" class="form-control shadow-none">
                         </div>
                         <div class="mb-3">
                             <label class="form-label">Email</label>
-                        <input type="email" class="form-control shadow-none">
+                            <input type="email" class="form-control shadow-none">
                         </div>
                         <div class="mb-3">
                             <label class="form-label">Subject</label>
-                        <input type="text" class="form-control shadow-none">
+                            <input type="text" class="form-control shadow-none">
                         </div>
                         <div class="mb-3">
                             <label class="form-label">Message</label>
                             <textarea class="form-control shadow-none" rows="5" style="resize : none;"></textarea>
                         </div>
-                            <button type="submit" class="btn btn-dark shadow-none">SEND</button>
+                        <button type="submit" class="btn btn-dark shadow-none">SEND</button>
                     </form>
                 </div>
             </div>
-        </div>
-    </div>
+
 
             <?php require('inc/footer.php') ?>
 
