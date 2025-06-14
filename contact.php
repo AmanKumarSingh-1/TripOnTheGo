74f5ff1f5078a15d5c128f6406b7615df929ce6d
Aman Kumar Singh <136615596+AmanKumarSingh-1@users.noreply.github.com>
2025-06-14 18:35:42 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,10 +1,3 @@
-<?php 
-  require('admin/inc/essentials.php');
-  require('admin/inc/db_config.php');
-?>
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
