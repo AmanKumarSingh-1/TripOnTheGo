a2d947f29e9960eee7267cb129b6da8afda57ed6
Aman Kumar Singh <contact.amankumarsingh@gmail.com>
2025-06-14 10:59:37 +0530

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -1,4 +1,16 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
+<?php
+    require('admin/inc/db_config.php');
+    require('admin/inc/essentials.php');
+
+    $contact_q = " SELECT * FROM `contact_details` WHERE `sr_no`=?";
+    $values = [1];
+    $contact_r = mysqli_fetch_assoc(select($contact_q, $values, 'i'));
+
+?>
+
+
+
+<nav  id= "nav-bar" class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
     <div class="container-fluid">
         <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">TripOnTheGo</a>
         <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,7 +19,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active me-2" aria-current="page" href="index.php">Home</a>
+                    <a class="nav-link me-2" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link me-2" href="rooms.php">Rooms</a>
@@ -21,11 +33,15 @@
                 <li class="nav-item">
                     <a class="nav-link me-2" href="about.php">About</a>
                 </li>
+                
+               
             </ul>
             <div class="d-flex">
                 <button type="button" class="btn btn-outline-dark shadow-none me-lg-3 me-2" data-bs-toggle="modal" data-bs-target="#loginModal">Login</button>
+
             </div>
             <div class="d-flex">
+
                 <button type="button" class="btn btn-outline-dark shadow-none " data-bs-toggle="modal" data-bs-target="#registerModal">Register</button>
             </div>
         </div>
