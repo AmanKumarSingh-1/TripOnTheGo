285830bfa3c4e12ec2feab53abe55fa98b61b506
Aman Kumar Singh <contact.amankumarsingh@gmail.com>
2025-06-14 10:58:47 +0530

    Update about.php

--- about.php
+++ about.php
@@ -1,40 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TripOnTheGo - About</title>
-    
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css"/>
-    <?php require('inc/links.php');?>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <?php require('inc/links.php'); ?>
     <style>
-        .box{
-            border-top-color: var(--teal)!important;
+        .box {
+            border-top-color: var(--teal) !important;
         }
     </style>
-    
-    
+
 </head>
+
 <body class="bg-light">
 
-    <?php require('inc/header.php');?>
+    <?php require('inc/header.php'); ?>
 
     <div class="my-5 px-4">
         <h2 class="fw-bold h-font text-center">ABOUT US</h2>
         <div class="h-line bg-dark"></div>
-        <p class="text-center mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quae, odit possimus incidunt harum facilis libero vel voluptates voluptate labore nemo esse obcaecati error magni ipsum porro aliquid distinctio id?
-        </p>
+        <p class="text-center mt-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quae, odit possimus incidunt harum facilis libero vel voluptates voluptate labore nemo esse obcaecati error magni ipsum porro aliquid distinctio id?</p>
     </div>
 
     <div class="container">
         <div class="row justify-content-between align-items-center">
             <div class="col-lg-6 col-md-5 mb-4 order-lg-1 order-md-1 order-2">
                 <h3 class="mb-3">Lorem ipsum dolor sit.</h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur illum similique magnam repudiandae deserunt ad at.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur illum similique magnam repudiandae deserunt ad at.
-                </p>
-                
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur illum similique magnam repudiandae deserunt ad at.</p>
             </div>
             <div class="col-lg-5 col-md-5 mb-4 order-lg-2 order-md-2 order-1">
                 <img src="images/about/about.jpg" class="w-100">
@@ -46,76 +42,65 @@
         <div class="row">
             <div class="col-lg-3 col-md-6 mb-4 px-4">
                 <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
-                    <img src="images/about/hotel.svg" width="70 px">
+                    <img src="images/about/hotel.svg" width="70px">
                     <h4 class="mt-3">100+ ROOMS</h4>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 mb-4" px-4>
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
                 <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
-                    <img src="images/about/customers.svg" width="70 px">
+                    <img src="images/about/customers.svg" width="70px">
                     <h4 class="mt-3">200+ CUSTOMERS</h4>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 mb-4" px-4>
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
                 <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
-                    <img src="images/about/rating.svg" width="70 px">
+                    <img src="images/about/rating.svg" width="70px">
                     <h4 class="mt-3">150+ REVIEWS</h4>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 mb-4" px-4>
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
                 <div class="bg-white rounded shadow p-4 border-top border-4 text-center box">
-                    <img src="images/about/staff.svg" width="70 px">
+                    <img src="images/about/staff.svg" width="70px">
                     <h4 class="mt-3">200+ STAFFS</h4>
                 </div>
             </div>
-            
         </div>
     </div>
-    
+
     <h3 class="my-5 fw-bold h-font text-center">MANAGEMENT TEAM</h3>
 
     <div class="container px-4">
         <div class="swiper mySwiper">
             <div class="swiper-wrapper mb-5">
-                <div class="swiper-slide bg-white text-center overflow.hidden rounded">
-                    <img src="images/about/team.jpg" class="w-100">
-                    <h5 class="mt-2">Random Name</h5>
-                </div>
-                <div class="swiper-slide bg-white text-center overflow.hidden rounded">
-                    <img src="images/about/team.jpg" class="w-100">
-                    <h5 class="mt-2">Random Name</h5>
-                </div>
-                <div class="swiper-slide bg-white text-center overflow.hidden rounded">
-                    <img src="images/about/team.jpg" class="w-100">
-                    <h5 class="mt-2">Random Name</h5>
-                </div>
-                <div class="swiper-slide bg-white text-center overflow.hidden rounded">
-                    <img src="images/about/team.jpg" class="w-100">
-                    <h5 class="mt-2">Random Name</h5>
-                </div>
-                <div class="swiper-slide bg-white text-center overflow.hidden rounded">
-                    <img src="images/about/team.jpg" class="w-100">
-                    <h5 class="mt-2">Random Name</h5>
-                </div>
-                <div class="swiper-slide bg-white text-center overflow.hidden rounded">
-                    <img src="images/about/team.jpg" class="w-100">
-                    <h5 class="mt-2">Random Name</h5>
-                </div>
                 
+                <?php
+                $about_r = selectAll('team_details');
+                $path = ABOUT_IMG_PATH;
+
+                while ($row = mysqli_fetch_assoc($about_r)) {
+                    echo <<<data
+                <div class="swiper-slide bg-white text-center overflow.hidden rounded">
+                    <img src="$path$row[picture]" class="w-100">
+                    <h5 class="mt-2">$row[name]</h5>
+                </div>
+                data;
+                }
+                 ?>
+
             </div>
             <div class="swiper-pagination"></div>
         </div>
     </div>
 
-    <?php require('inc/footer.php')?>
+    <?php require('inc/footer.php') ?>
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
     <script>
         var swiper = new Swiper(".mySwiper", {
-            spaceBetween:40,
+            spaceBetween: 40,
             pagination: {
-            el: ".swiper-pagination",
+                el: ".swiper-pagination",
             },
             breakpoints: {
                 320: {
@@ -134,5 +119,5 @@
         });
     </script>
 </body>
-</html>
 
+</html>
